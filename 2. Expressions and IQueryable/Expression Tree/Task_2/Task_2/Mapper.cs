0d7f63e5f92149e14450439fd60f27375459fac7
Артём <artyom.bratolyubov@gmail.com>
2018-10-15 20:19:14 +0300

    add mapper file

---
+++ 2. Expressions and IQueryable/Expression Tree/Task_2/Task_2/Mapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Task_2
+{
+	public class Mapper<TSource, TDestination>
+	{
+		Func<TSource, TDestination> mapFunction;
+
+		internal Mapper(Func<TSource, TDestination> func)
+		{
+			mapFunction = func;
+		}
+
+		public TDestination Map(TSource source)
+		{
+			return mapFunction(source);
+		}
+	}
+
+	public class MappingGenerator
+	{
+		public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
+		{
+			var createdType = typeof(TDestination);
+
+			var sourceParam = Expression.Parameter(typeof(TSource));
+			var sourceProp = Expression.Property(sourceParam, "FullName");
+
+			var destValueProperty = createdType.GetProperty("Name");
+			var displayValueAssignment = Expression.Bind(destValueProperty, sourceProp);
+
+			var ctor = Expression.New(createdType);
+			var memberInit = Expression.MemberInit(ctor, displayValueAssignment);
+
+			var mapFunction = Expression.Lambda<Func<TSource, TDestination>>(memberInit, sourceParam);
+			Console.WriteLine(mapFunction);
+			return new Mapper<TSource, TDestination>(mapFunction.Compile());
+		}
+	}
+}
